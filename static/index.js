250be0e5ff4f37f9d88ce64b20327e5d681242ca
Jim Pick <jim@backblend.com>
2019-07-17 18:32:20 -0700

    Mockup with results displayed

--- static/index.js
+++ static/index.js
@@ -1,35 +1,40 @@
 import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.mjs'
 
 const baseUrl = 'https://mockup-3knoa5s4ea-uc.a.run.app/mockup'
+// const baseUrl = 'http://127.0.0.1:8001/mockup'
 
-const metricsUrl = baseUrl + '/metrics.json?_shape=array'
-const commitsUrl = baseUrl + '/commits.json?_shape=array'
+const testsUrl = baseUrl + '/test.json?_shape=array'
+const metricsUrl = baseUrl + '/metric.json?_shape=array'
+const commitsUrl = baseUrl + '/commit.json?_shape=array'
 
-const query = 'select distinct commits.*, results.metricId,' + 
-  'metrics.description, results.value, metrics.unit ' +
-  'from commits, results, metrics ' +
-  'where commits.sha = results.sha and ' +
-  'results.metricId = metrics.id and ' +
-  'results.sha = commits.sha'
-
-const url = baseUrl + '.json?sql=' +
-  encodeURIComponent(query) + '&_shape=array'
+const query = sql => baseUrl + '.json?sql=' + encodeURIComponent(sql) +
+  '&_shape=array'
+const testSuitesUrl = query('select slug, name from test_suite ' +
+  'order by sort_order, slug')
+const resultsUrl = query('select result.*, test_run.* from result, test_run ' +
+  'where result.test_run_id = test_run.id')
 
 class Dashboard extends Component {
   componentDidMount () {
-    fetch(url)
+    fetch(testSuitesUrl)
       .then(resp => resp.json())
-      .then(data => this.setState({ data }))
+      .then(testSuites => this.setState({ testSuites }))
+    fetch(testsUrl)
+      .then(resp => resp.json())
+      .then(tests => this.setState({ tests }))
     fetch(metricsUrl)
       .then(resp => resp.json())
       .then(metrics => this.setState({ metrics }))
     fetch(commitsUrl)
       .then(resp => resp.json())
       .then(commits => this.setState({ commits }))
+    fetch(resultsUrl)
+      .then(resp => resp.json())
+      .then(results => this.setState({ results }))
   }
 
-  render (props, { data, metrics, commits }) {
-    if (metrics && commits) {
+  render (_, { testSuites, tests, metrics, commits, results }) {
+    if (testSuites && tests && metrics && commits && results) {
       const commitHeaders = commits.map(commit => {
         const [date, time] = commit.date.replace(' +0000 UTC', '').split(' ')
         return html`
@@ -54,14 +59,76 @@ class Dashboard extends Component {
           </th>
         `
       })
+      const rows = []
+      for (const testSuite of testSuites) {
+        const { slug: testSuiteSlug, name: testSuiteName } = testSuite
+        rows.push(html`
+          <tr class="suite">
+            <td colspan=${commits.length + 1}>
+              <div>${testSuiteName}</div>
+            </td>
+          </tr>
+        `)
+        const filteredTests = tests
+          .filter(test => test.test_suite_slug === testSuiteSlug)
+          .sort((a, b) => a.sort_order - b.sort_order)
+        filteredTests.forEach(test => {
+          const dataCols = commits.map(commit => {
+            return html`<td></td>`
+          })
+          rows.push(html`
+            <tr
+              class="test"
+              data-test-slug=${test.slug}
+              data-test-description=${test.description}
+            >
+              <td>${test.name}</td>
+              ${dataCols}
+            </tr>
+          `)
+
+          const filteredMetrics = metrics
+            .filter(metric => metric.test_suite_slug === testSuiteSlug &&
+              metric.test_slug === test.slug)
+            .sort((a, b) => a.sort_order - b.sort_order)
+          filteredMetrics.forEach(metric => {
+            const dataCols = commits.map(commit => {
+              // FIXME: This is inefficient
+              const result = results.filter(result =>
+                result.sha === commit.sha &&
+                result.test_suite_slug === testSuiteSlug &&
+                result.test_slug === test.slug &&
+                result.metric_slug === metric.slug
+              )
+              if (result.length === 1) {
+                return html`<td>${result[0].value}s</td>`
+              }
+              return html`<td></td>`
+            })
+            rows.push(html`
+              <tr
+                class="metric"
+                data-test-slug=${test.slug}
+                data-test-description=${test.description}
+              >
+                <td>${metric.slug}</td>
+                ${dataCols}
+              </tr>
+            `)
+          })
+
+        })
+      }
+      /*
       const rows = metrics.map(metric => {
         const dataCols = commits.map(commit => {
           return html`<td>.</td>`
         })
         return html`
-          <tr><td>${metric.id}</td>${dataCols}</tr>
+          <tr><td>${metric.slug}</td>${dataCols}</tr>
         `
       })
+      */
       return html`
         <div>
           <div class="tracking-issue">
@@ -76,12 +143,15 @@ class Dashboard extends Component {
         </div>
       `
     }
+    return html`<div>Loading...</div>`
+    /*
     return html`
       <div>
         Dashboard
         <pre>${commits && JSON.stringify(commits, null, 2)}</pre>
       </div>
     `
+    */
   }
 }
 
